9f86ba978eed49f35b894330749f9ea1257c1352
MDEGORRE <degorremichael@gmail.com>
2025-04-03 14:35:43 +0200

    added no translate meta tag for google

--- app.vue
+++ app.vue
@@ -19,8 +19,8 @@ useHead({
     meta: [
     { name: 'description', content: 'Website about items datas from the Final Fantasy XIV game. Helps players making profit or saving gils.' },
     { name: 'author', content: "Michaël Degorre" },
-    { name: 'keywords', content: "Market, MarketBoard, Prices, Items, Final Fantasy XIV, FFXIV, Square Enix, Universalis, Treasure maps, Potions, Raids, Gils, Gears, Materias, Foods" }
-    
+    { name: 'keywords', content: "Final Fantasy XIV Market, Market, MarketBoard, Prices, Items, Final Fantasy XIV, FFXIV, Square Enix, Universalis, Treasure maps, Potions, Raids, Gils, Gears, Materias, Foods" },
+    { name: "google", content: "notranslate" }
     ],
     bodyAttrs: {
     	class: "bg-[url('https://lds-img.finalfantasyxiv.com/h/I/RnhxiLA0fuKtHd4bywd8OzzHcM.jpg')] lg:bg-[url('https://lds-img.finalfantasyxiv.com/h/F/QAim605TH1YRvUi9cFGReikYoI.jpg')] bg-cover bg-center"
